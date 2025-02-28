cd79bed5987b1ecb315b9ed28896aa6686f01938
ZagainovNA <n.zagainov@id-mt.ru>
2025-02-28 15:59:13 +0300

    add auth

---
+++ src/main/java/com/example/podvezi/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.example.podvezi.repository;
+
+import com.example.podvezi.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+}
