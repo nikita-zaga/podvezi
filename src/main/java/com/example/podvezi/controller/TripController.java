8d3fc9a72253e980c7f3451570afdc8815a9e7d7
ZagainovNA <n.zagainov@id-mt.ru>
2025-03-06 13:18:59 +0300

    add new controller and method for create trips

--- src/main/java/com/example/podvezi/controller/TripController.java
+++ src/main/java/com/example/podvezi/controller/TripController.java
@@ -1,13 +1,29 @@
 package com.example.podvezi.controller;
 
+import com.example.podvezi.dto.CreateDriverTripDto;
+import com.example.podvezi.dto.RegistrationDto;
+import com.example.podvezi.service.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
+@RequestMapping("/trip")
 public class TripController {
     private static final Logger logger = LoggerFactory.getLogger(TripController.class);
 
+    @Autowired
+    private TripService tripService;
 
+    @PostMapping("/create/driver")
+    public ResponseEntity<?> createDriverTrip(@RequestBody CreateDriverTripDto createDriverTripDto) {
+        tripService.createDriverTrip(createDriverTripDto);
 
+        return ResponseEntity.ok("Поездка создана успешно");
+    }
 }
--- src/main/java/com/example/podvezi/model/Trip.java
+++ src/main/java/com/example/podvezi/model/Trip.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
@@ -18,13 +19,24 @@ public class Trip {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String startLocation;
-    private String endLocation;
+    private String route;
     private LocalDateTime startTime;
     private String price;
     private String countFreePlaces;
     private String status;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Car car;
+    private Driver driver;
+
+    public Trip(String route, LocalDateTime startTime, String price, String countFreePlaces, String status, Driver driver) {
+        this.route = route;
+        this.startTime = startTime;
+        this.price = price;
+        this.countFreePlaces = countFreePlaces;
+        this.status = status;
+        this.driver = driver;
+    }
+
+    public Trip() {
+    }
 }
